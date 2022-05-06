0f04e07a461aac5ae5cf708b3f8f38b9652f4766
chomonolis <tyomo3317@gmail.com>
2022-05-06 17:34:17 +0900

    OutputRandの結果出力にOutputResultを使用

--- src/component/OutputRand.tsx
+++ src/component/OutputRand.tsx
@@ -4,25 +4,29 @@ import XORShift from '../class/XORShift';
 import SeedInput from './SeedInput';
 import OutputModRand from './SetMod';
 import XorsInfo from './XorsInfo';
+import OutputResult, { Result } from './OutputResult';
 
 const OutputRand = () => {
   const [xors, setXors] = useState<XORShift>(new XORShift(10));
-  const [printNumber, setPrintNumber] = useState<number>(-1);
-  const onClick = () => {
-    setPrintNumber(xors.modNext(100));
-  };
+  const [result, setResult] = useState<Result>();
 
   const modRandCallback = (mod: number) => {
-    setPrintNumber(xors.modNext(mod));
+    const res = xors.modNext(mod);
+    const r: Result = {
+      seed: xors.getSeed(),
+      count: xors.getCount(),
+      mod: mod,
+      res: res,
+    };
+    setResult(r);
   };
 
   return (
     <>
       <XorsInfo xors={xors} />
-      <button onClick={onClick}>next</button>
-      {printNumber}
       <SeedInput setXors={setXors} />
       <OutputModRand callbackFunc={modRandCallback} />
+      {result && <OutputResult result={result} />}
     </>
   );
 };
