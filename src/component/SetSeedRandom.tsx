82a8a0bb50f9f2250ae82f30cf49a30ba68955c5
chomonolis <tyomo3317@gmail.com>
2022-05-06 18:10:06 +0900

    ランダムシード決定コンポーネントの作成

---
+++ src/component/SetSeedRandom.tsx
@@ -0,0 +1,42 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { Button, TextField } from '@mui/material';
+
+import registerMui from '../utils/registerMui';
+import XORShift from '../class/XORShift';
+
+type RangeInputType = {
+  min: number;
+  max: number;
+};
+
+type Props = {
+  setXors: React.Dispatch<React.SetStateAction<XORShift>>;
+};
+
+const SetSeedRandom = (props: Props) => {
+  const { setXors } = props;
+  const { register, handleSubmit } = useForm<RangeInputType>();
+
+  const onSubmit: SubmitHandler<RangeInputType> = (data) => {
+    const p = data.max - data.min;
+    if (p > 0) {
+      const q = Math.floor(Math.random() * p);
+      setXors(new XORShift(q + data.min));
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField type='number' {...registerMui(register('max', { required: true }))} />
+        <TextField type='number' {...registerMui(register('min', { required: true }))} />
+        <Button variant='contained' type='submit' color='primary'>
+          ランダムシード値
+        </Button>
+      </form>
+    </>
+  );
+};
+
+export default SetSeedRandom;
