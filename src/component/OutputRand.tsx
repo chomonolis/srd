6ab989da5b438d8e3fad4066dbd8b5fbdaa51788
chomonolis <tyomo3317@gmail.com>
2022-05-06 16:21:18 +0900

    SeedInputの作成

--- src/component/OutputRand.tsx
+++ src/component/OutputRand.tsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 import XORShift from '../class/XORShift';
+import SeedInput from './SeedInput';
 
 const OutputRand = () => {
-  const [xors] = useState<XORShift>(new XORShift(10));
+  const [xors, setXors] = useState<XORShift>(new XORShift(10));
   const [printNumber, setPrintNumber] = useState<number>(-1);
   const onClick = () => {
     setPrintNumber(xors.modNext(100));
@@ -11,6 +12,7 @@ const OutputRand = () => {
     <>
       <button onClick={onClick}>next</button>
       {printNumber}
+      <SeedInput setXors={setXors} />
     </>
   );
 };
---
+++ src/component/SeedInput.tsx
@@ -0,0 +1,36 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { Button, TextField } from '@mui/material';
+
+import registerMui from '../utils/registerMui';
+import XORShift from '../class/XORShift';
+
+type SeedInputType = {
+  seed: number;
+};
+
+type Props = {
+  setXors: React.Dispatch<React.SetStateAction<XORShift>>;
+};
+
+const SeedInput = (props: Props) => {
+  const { setXors } = props;
+  const { register, handleSubmit } = useForm<SeedInputType>();
+  const onSubmit: SubmitHandler<SeedInputType> = (data) => {
+    console.log(data.seed);
+    setXors(new XORShift(data.seed));
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField type='number' {...registerMui(register('seed', { required: true }))} />
+        <Button variant='contained' type='submit' color='primary'>
+          シード値変更
+        </Button>
+      </form>
+    </>
+  );
+};
+
+export default SeedInput;
