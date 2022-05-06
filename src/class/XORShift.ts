005db85f1060dc161c2b04e9e3dff3ca1f89d13b
chomonolis <tyomo3317@gmail.com>
2022-05-06 16:56:24 +0900

    XORShiftクラスにseedとcountを追加。next時にcountを増加

--- src/class/XORShift.ts
+++ src/class/XORShift.ts
@@ -3,12 +3,17 @@ class XORShift {
   private y = 362436069;
   private z = 521288629;
   private w = 0;
+  private seed = 0;
+  private count = 0;
 
   constructor(seed: number) {
     this.w = seed;
+    this.seed = seed;
+    this.count = 0;
   }
 
   next() {
+    this.count++;
     const t = this.x ^ (this.x << 11);
     this.x = this.y;
     this.y = this.z;
@@ -21,6 +26,13 @@ class XORShift {
       return 0;
     }
     return this.next() % mod;
+  }
+
+  getSeed() {
+    return this.seed;
+  }
+  getCount() {
+    return this.count;
   }
 }
 
