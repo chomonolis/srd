ae2dc0b3ff2c82f4c3b29d3cca26222600ced639
chomonolis <tyomo3317@gmail.com>
2022-05-06 14:40:32 +0900

    XORShift処理を行うクラスの実装

---
+++ src/class/XORShift.ts
@@ -0,0 +1,24 @@
+class XORShift {
+  private x = 123456789;
+  private y = 362436069;
+  private z = 521288629;
+  private w = 0;
+
+  constructor(seed: number) {
+    this.w = seed;
+  }
+
+  next() {
+    const t = this.x ^ (this.x << 11);
+    this.x = this.y;
+    this.y = this.z;
+    this.z = this.w;
+    return Math.abs((this.w = this.w ^ (this.w >>> 19) ^ (t ^ (t >>> 8))));
+  }
+
+  modNext(mod: number) {
+    return this.next() % mod;
+  }
+}
+
+export default XORShift;
